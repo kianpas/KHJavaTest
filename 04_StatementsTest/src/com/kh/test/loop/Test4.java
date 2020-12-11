6e17cd6391e87c9bb471cbcbf71f2a78c71e51b6
kianpas <kianpas@gmail.com>
2020-12-11 12:46:20 +0900

    Test4 commit

--- 04_StatementsTest/src/com/kh/test/loop/Test4.java
+++ 04_StatementsTest/src/com/kh/test/loop/Test4.java
@@ -15,31 +15,43 @@ public class Test4 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("문자열 입력 : ");
-		
 		String str = sc.nextLine();
 		
 		System.out.print("검색할 문자 입력 : ");
+		char ch = sc.next().charAt(0);
 		
-		//char ch = sc.next().charAt(0);
-		String res = sc.nextLine();
+		int index = 0;
+		//length가 5라면 위치는 4		
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == ch) {
+				index++; // 무한 제어
+			} 
+		}
+		if ('a' <=ch && ch <= 'z') {
+			System.out.println("'" + ch + "'" + "가 포함된 갯수 : " + index + "개");
+		} else {
+			System.out.println("영문자가 아닙니다.");
+		}
+	}	
+}
 		
-		int index = str.indexOf(res);
-//		
+		
+//		String res = sc.nextLine();
+		
+		
+//		char charAt(int location);
+		
 //		System.out.println(index);
 
 		//문자열 1이 증가할때마다 검사?
-		for(int i = 0; i <= str.length() ; i++) {
-//			if()
 			
-			
-			
-			System.out.println(index);
+//			}
 //			if(index ) {
 //				str.length = 0 > ch == 
 //			
 //				System.out.println("'" + ch + "'" + "가 포함된 갯수 : ");
 		//	}
 	
-		}
-	}
-}
+//		}
+//	}
+//}
