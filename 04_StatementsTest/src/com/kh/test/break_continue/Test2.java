6273e065e3ff36d7f830fffb3f1506d93e14044a
kianpas <kianpas@gmail.com>
2020-12-13 15:27:26 +0900

    구간 판정 구문 추가

--- 04_StatementsTest/src/com/kh/test/break_continue/Test2.java
+++ 04_StatementsTest/src/com/kh/test/break_continue/Test2.java
@@ -10,9 +10,41 @@ public class Test2 {
 		Test2 t2 = new Test2();
 		//t2.test();
 		//t2.test2();
-		t2.test3();
+		//t2.test3();
+		t2.test4();
 		
 
+	}
+	public void test4() {
+		int rnd = (int) (Math.random()*100+1);
+		Scanner sc = new Scanner(System.in);
+		
+		int cnt = 0;
+	
+		while(true) {
+			System.out.print("정수 입력 (1 ~ 100) : ");
+			int num = sc.nextInt();
+				cnt++;
+				
+			if (num > 100 || num < 1) {
+					System.out.println("잘 못 입력하셨습니다.");
+					continue;
+			}	
+								
+			if(num < rnd) {
+				System.out.println("입력하신 정수보다 큽니다.");
+				continue;
+			}
+			if (num > rnd){
+				System.out.println("입력하신 정수보다 작습니다.");
+				continue;
+			}
+			
+			if (num == rnd) {
+				System.out.println("정답입니다." + cnt + "회에 만에 정답을 맞추셨습니다." );
+				break;
+			}
+		}
 	}
 	public void test3() {
 		Random rnd = new Random();
