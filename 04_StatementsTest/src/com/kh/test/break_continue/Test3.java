3a3e6b24a150a873a45f83be8de2daf67eb2cc0e
kianpas <kianpas@gmail.com>
2020-12-15 13:44:50 +0900

    12.15 test3 소수구하기 수정

--- 04_StatementsTest/src/com/kh/test/break_continue/Test3.java
+++ 04_StatementsTest/src/com/kh/test/break_continue/Test3.java
@@ -9,88 +9,134 @@ public class Test3 {
 		Test3 t3 = new Test3();
 		//t3.test();
 		//t3.test2();
-		t3.test3();
+		//t3.test3();
+		t3.test4();
 	}
 
-	public void test3() {
+	public void test4() {
+		
 		Scanner sc = new Scanner(System.in);
-		
 		int num;
-		
-		do {
+		boolean flag = true;
+		while(true) {
 			System.out.print("숫자 입력 : ");
 			num = sc.nextInt();
 			
 			if(num < 2) {
 				System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
 					continue;
+			} else {
+				break;
 			}
+		}
+		//소수는 2부터 num/2 까지 나누었을 때 나누어지는 숫자가 존재하지 않으면 소수
+		//num의 약수는 num/2보다 클수 없으므로 2부터 루트 num
+		//루트 표현위해 j*j<=i
 			
-			int i = 1;
-			while(i < num) {
-					i++;
-				if(num % 2 != 0) {
-					System.out.println(num + "은 소수다.");
+		for(int i = 2; i<=num; i++) {
+			for(int j = 2; j <= i/2; j++) {
+				if(i%j == 0) {
+					flag = false;
 					break;
-				}else 					
-					System.out.println(num + "은 소수아니다.");
+				} else 
+					flag = true;
 					break;
-			}	break;
-			
-		} while(true);
-		
+			}
+		}
+			if(flag) {
+				System.out.println("소수");
+				
+			} else {
+				System.out.println("소수 아님");
+			}
 	}
+		
+	//이하 버림
+	
+	
 
-	public void test2() {
-		
-		Scanner sc = new Scanner(System.in);
-		int num;
-		
-		for(int i = 0; ; i++) {
-			System.out.print("숫자 입력 : ");
-			num = sc.nextInt();
-			if(num>2)
-				break;
-			else
-				System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
-				continue;
-		}
-		for(int i = 2; ; i++) {
-			if(num % 2 != 0) {
-				System.out.println("소수다.");
-				break;
-			} else 
-				System.out.println("소수가 아니다");
-				break;
-			
-		}
-	}
-
-	public void test() {
-		
-		Scanner sc = new Scanner(System.in);
-		int num;
-		
-		while(true) {
-			System.out.print("숫자 입력 : ");
-			num = sc.nextInt();		//i++이 컨티뉴 뒤에 있는 경우는 무한 반복
-			if(num > 2)  	
-				break;			
-			else 
-				System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");//
-				continue;
-		}
-		int i = 1;
-		while(i <= num) {
-			i++;
-			if(num % 2 != 0) {
-				System.out.println("소수다.");
-				break;
-			} else 
-				System.out.println("소수가 아니다.");
-				break;
-		}
-	}
+//	public void test3() {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int num;
+//		
+//		
+//		do {
+//			System.out.print("숫자 입력 : ");
+//			num = sc.nextInt();
+//			
+//			if(num < 2) {
+//				System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
+//					continue;
+//			}
+//			//3부터 2씩증가하는 수(홀수)로 차례로 나누어본다.
+//			//나누어지는 수가 있으면, 소수가 아니다.
+//			//나누어 지는 수가 없으면, 1과 본인밖에 약수가 없으므로 소수이다. 
+//			
+//			int i = 1;
+//			while(i < num) {
+//					i++;
+//				if(num % 2 != 0) {    //2로 나누면 홀수 나타낸다는 말
+//					System.out.println(num + "은 소수다.");
+//					break;
+//				}else 					
+//					System.out.println(num + "은 소수아니다.");
+//					break;
+//			}	break;
+//			
+//		} while(true);
+//		
+//	}
+//
+//	public void test2() {
+//		
+//		Scanner sc = new Scanner(System.in);
+//		int num;
+//		
+//		for(int i = 0; ; i++) {
+//			System.out.print("숫자 입력 : ");
+//			num = sc.nextInt();
+//			if(num>2)
+//				break;
+//			else
+//				System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
+//				continue;
+//		}
+//		for(int i = 2; ; i++) {
+//			if(num % 2 != 0) {
+//				System.out.println("소수다.");
+//				break;
+//			} else 
+//				System.out.println("소수가 아니다");
+//				break;
+//		}
+//	}
+//
+//	public void test() {
+//		
+//		Scanner sc = new Scanner(System.in);
+//		int num;
+//		
+//		while(true) {
+//			System.out.print("숫자 입력 : ");
+//			num = sc.nextInt();		//i++이 컨티뉴 뒤에 있는 경우는 무한 반복
+//			if(num > 2)  	
+//				break;			
+//			else 
+//				System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");//
+//				continue;
+//		}
+//		int i = 1;
+//		while(i <= num) {
+//			i++;
+//			if(num % i != 0) {
+//				System.out.println("소수다.");
+//				break;
+//			} else 
+//				System.out.println("소수가 아니다.");
+//				break;
+//		}
+//	}
 }
 	
 		
